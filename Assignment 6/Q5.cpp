ace23276c30208abc60a885f0311252dcef778b1
Abhinav622-tech <agupta33_be24@thapar.edu>
2025-11-30 14:56:06 +0530

    Implement circular linked list detection
    Added functionality to check if a linked list is circular and tested it with both circular and non-circular linked lists.

--- Assignment 6/Q5.cpp
+++ Assignment 6/Q5.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
 using namespace std;
-class ListNode{
-    public:
+
+class ListNode {
+public:
     int val;
     ListNode *next;
-    ListNode(int val1){
-        val=val1;
-        next=nullptr;
+
+    ListNode(int val1) {
+        val = val1;
+        next = nullptr;
     }
-    ListNode(int val1, ListNode* next1){
-        val=val1;
-        next=next1;
+
+    ListNode(int val1, ListNode* next1) {
+        val = val1;
+        next = next1;
     }
-    bool iscircular(ListNode* head){
-        if(head==NULL || head->next==NULL) return false;
-        if(head->next==head) return true;
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(fast==slow) return true;
+
+    // CHECK IF LINKED LIST IS CIRCULAR
+    bool iscircular(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
         }
         return false;
     }
-    
 };
-int main()
-{
-    
-    return 0; 
+
+int main() {
+
+    // -----------------------
+    // NON-CIRCULAR LINKED LIST
+    // -----------------------
+    ListNode* head1 = new ListNode(1);
+    head1->next = new ListNode(2);
+    head1->next->next = new ListNode(3);
+
+    ListNode obj(0);
+    cout << "List 1 Circular? " 
+         << (obj.iscircular(head1) ? "Yes" : "No") 
+         << endl;
+
+    // -----------------------
+    // CIRCULAR LINKED LIST
+    // -----------------------
+    ListNode* head2 = new ListNode(10);
+    head2->next = new ListNode(20);
+    head2->next->next = new ListNode(30);
+
+    // make a loop
+    head2->next->next->next = head2;  // last node pointing to head
+
+    cout << "List 2 Circular? " 
+         << (obj.iscircular(head2) ? "Yes" : "No") 
+         << endl;
+
+    return 0;
 }
